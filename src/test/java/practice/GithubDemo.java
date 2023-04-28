f7ab62e778b68e71c22233aa1bcd4dab53d00cb4
sachi <sachi@PureBook-S14>
2023-04-28 12:57:21 +0530

    ok

--- src/test/java/practice/GithubDemo.java
+++ src/test/java/practice/GithubDemo.java
@@ -4,6 +4,8 @@ public class GithubDemo {
 	public static void main(String[] args) {
 		System.out.println("hello killer");
 		System.out.println("updated and saved");
+		System.out.println("mama");
+		
 	}
 
 }
