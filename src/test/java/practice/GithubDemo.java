089cc46049b07ebf91f8ed45e1d75a7af0bb4c0b
sachin <sachinpasargi143@gmail.com>
2023-04-28 12:44:57 +0530

    commit and push

--- src/test/java/practice/GithubDemo.java
+++ src/test/java/practice/GithubDemo.java
@@ -3,7 +3,7 @@ package practice;
 public class GithubDemo {
 	public static void main(String[] args) {
 		System.out.println("hello killer");
-		System.out.println("updated");
+		System.out.println("updated and saved");
 	}
 
 }
